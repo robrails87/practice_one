dc28cc1b2bc9ff835fbf32f4da7be27a9e670ace
Robert Pliszka <robrails87@gmail.com>
2013-03-28 14:36:59 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeOne::Application.config.secret_token = 'bd97a202868af58a0713fd68c0ef3de42c2591a52df5708c0cf70d6bcbbc3cd29b3c51ff2bebc4c2bb284805f92f4e2acdbface6dc6058d753966960ca85cf69'
